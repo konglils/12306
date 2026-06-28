367ab5400c69cd0970e5fb6f2eebe551451b918e
konglils <mingjuexinling@gmail.com>
2026-06-28 23:44:32 +0800

    build: 创建 Spring Boot 和 Maven 后端环境

---
+++ backend/src/test/java/cn/nispring/_12306/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nispring._12306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

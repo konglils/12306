ebd4153dc17428b2c0ed4b8d2b6f61d480b13486
konglils <mingjuexinling@gmail.com>
2026-07-22 15:22:21 +0800

    feat: 删除旧的数据类型

--- backend/src/main/java/cn/nispring/rail12306/model/ErrorResponse.java
+++
@@ -1,3 +0,0 @@
-package cn.nispring.rail12306.model;
-
-public record ErrorResponse(String message) {}
--- backend/src/main/java/cn/nispring/rail12306/model/Seat.java
+++
@@ -1,4 +0,0 @@
-package cn.nispring.rail12306.model;
-
-public record Seat(String type, int price, int remaining) {
-}

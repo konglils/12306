47a1db0d0d68d15495248f5cd390ef42d9eda399
konglils <mingjuexinling@gmail.com>
2026-09-03 23:03:26 +0800

    feat: 添加 CarLayoutMapper

---
+++ backend/src/main/java/cn/nispring/rail12306/mapper/CarLayoutMapper.java
@@ -0,0 +1,36 @@
+package cn.nispring.rail12306.mapper;
+
+import cn.nispring.rail12306.entity.CarLayoutEntity;
+import cn.nispring.rail12306.entity.StopEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Mapper
+public interface CarLayoutMapper {
+
+    @Delete("DELETE FROM car_layouts WHERE train_date < CURRENT_DATE()")
+    void deleteOld();
+
+    @Insert("""
+            <script>
+            INSERT INTO car_layouts
+                (train_date, train_id, car_id, layout)
+            VALUES
+            <foreach collection="list" item="entity" separator=",">
+                (#{entity.trainDate}, #{entity.trainId}, #{entity.carId}, #{entity.layout})
+            </foreach>
+            </script>
+            """)
+    int insertBatch(List<CarLayoutEntity> entities);
+
+    @Select("SELECT EXISTS(SELECT 1 FROM car_layouts WHERE train_date = #{date})")
+    boolean existsByDate(LocalDate date);
+
+    @Select("SELECT (train_date, train_id, car_id, layout) FROM car_layouts WHERE train_date = #{date} AND train_id = #{trainId}")
+    CarLayoutEntity selectByTrainId(LocalDate date, Long trainId);
+
+    @Select("SELECT (train_date, train_id, car_id, layout) FROM car_layouts WHERE train_date = #{date} AND car_id = #{carId}")
+    List<CarLayoutEntity> selectByCarId(LocalDate date, Long carId);
+}

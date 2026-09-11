ffd7c7d24dc33aa269d9ede921ef377843feb28b
konglils <mingjuexinling@gmail.com>
2026-09-11 23:40:17 +0800

    feat(back): add Passenger

---
+++ backend/src/main/java/cn/nispring/rail12306/model/Passenger.java
@@ -0,0 +1,20 @@
+package cn.nispring.rail12306.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record Passenger(
+        @NotNull(message = "证件类型不能为空") IdType idType,
+        @NotBlank(message = "证件号码不能为空") String idNo,
+        @NotBlank(message = "姓名不能为空") String name,
+        String phone,
+        String email,
+        String countryCode,
+        LocalDate birthDate,
+        Sex sex,
+        LocalDate validThrough,
+        @NotNull(message = "优惠类型不能为空") DiscountType discountType
+) {
+}
